3b776b2928c582f0c82a982de8c3bd39e8c96c61
Scolastica Nzyoki <solanzioki18@gmail.com>
2024-11-30 13:15:52 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph")
+   return cats;
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob")
+    return cats;
+}
+ function destructivelyRemoveLastCat(Garfield){
+    cats.pop("Garfield")
+ }
+ function destructivelyRemoveFirstCat(){
+    cats.shift()
+ }
+ function appendCat(Broom){
+    //cats.push("Broom")
+   return[...cats,"Broom"]
+ }
+ function prependCat(){
+    return ["Arnold",...cats]
+ }
+ function removeLastCat(){
+   return cats.slice(0,-1)
+ }
+ function removeFirstCat(){
+    return cats.slice(1)
+ }
